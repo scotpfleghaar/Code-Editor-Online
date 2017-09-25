edb1a500c4b63f670b3ffc1e5c75bce1d005c3ab
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-09-25 16:07:17 -0500

    Created an in browser text editor for the code challanger.
    -Working with ace.js and beautify.js to make a inbrowser text editor.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 // A $( document ).ready() block.
 $(document).ready(function () {
-    var text = $('pre').text();
-    console.log(text);
+    var editor = ace.edit("editor");
+    //editor.setTheme("ace/theme/twilight");
+    editor.session.setMode("ace/mode/javascript");
+
+    // Appears to only work with php
+    // var beautify = ace.require("ace/ext/beautify"); // get reference to extension
+    // beautify.beautify(editor.session);
+    function beatify() {
+        var val = editor.session.getValue();
+        //Remove leading spaces
+        var array = val.split(/\n/);
+        array[0] = array[0].trim();
+        val = array.join("\n");
+        //Actual beautify (prettify) 
+        val = js_beautify(val);
+        //Change current text to formatted text
+        editor.session.setValue(val);
+    }
+    beatify();
 });
