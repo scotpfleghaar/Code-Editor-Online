74aad458838a100c1156872006de9884553047af
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-09-26 15:27:57 -0500

    -Added more logic
    -The functionality is in place, it is working wonderfully, I ran into an error in my lexical scope, but unbinding the function click solved the issue. Next, refactor, add questions, and add more features.

--- dynamicLearning.js
+++ dynamicLearning.js
@@ -1,75 +1,243 @@
 // Copyright Scot Pfleghaar 2017
-var question = [{
-    questionNum: 1,
-    questionInstructions: `Where does the $.ajax go?`,
-    question: `$("button").click(function(){$.ajax({url: "demo_test.txt", success: function(result){$("#div1").html(result);}});});`,
-    answer: `$("button").click(function(){$.ajax({url: "demo_test.txt", success: function(result){$("#div1").html(result);}});});`,
-    timeCorrect: 0
-}, {
-    questionNum: 1,
-    questionInstructions: `Where does the $.get go?`,
-    question: `$("button").click(function(){ ("demo_test.asp", function(data, status){  alert("Data: " + data + "\nStatus: " + status); });});`,
-    answer: `$("button").click(function(){ $.get("demo_test.asp", function(data, status){  alert("Data: " + data + "\nStatus: " + status); });});`,
-    timeCorrect: 0
-}, {
-    questionNum: 1,
-    questionInstructions: `Where does the $.post go?`,
-    question: `$("button").click(function(){("demo_test.asp", function(data, status){alert("Data: " + data + "\nStatus: " + status);});});`,
-    answer: `$("button").click(function(){$.post("demo_test.asp", function(data, status){alert("Data: " + data + "\nStatus: " + status);});});`,
-    timeCorrect: 0
-}, {
-    questionNum: 1,
-    questionInstructions: `Load the content of the file "demo_test.txt" into a specific <div> element:`,
-    question: `$("button").click(function(){});`,
-    answer: `$("button").click(function(){$("#div1").load("demo_test.txt");});`,
-    timeCorrect: 0
-}, {
-    questionNum: 1,
-    questionInstructions: `Output the result of serialized form values:`,
-    question: `$("button").click(function(){$("div").text($("form"));});`,
-    answer: `$("button").click(function(){$("div").text($("form").serialize());});`,
-    timeCorrect: 0
-}]
-
-//Custom Functions
-function trimmer(value) {
-    value = value.replace(/\s/g, '');
-    value = value.replace(/[\t\n]+/g, '')
-    value = value.replace(/(\r\n|\n|\r)/gm, '')
-    return value;
-}
-
-// Display the first question
-var instructions = question[0].questionInstructions
-var questions = question[0].question
-var answers = question[0].answer
-
-var instructions = $.trim(instructions);
-var questions = trimmer(questions);
-var answers = trimmer(answers);
+//IIFE to protect code
+(function () {
 
 
-console.log(instructions);
-console.log(questions);
-console.log(answers);
+    var question = [{
+        questionNum: 1,
+        questionInstructions: `Select all elements inside the document: (Use Double Quotes)`,
+        question: ``,
+        answer: `$("*");`,
+        timeCorrect: 0
+        }, {
+        questionNum: 2,
+        questionInstructions: `Select the element with the id "intro": (Use Double Quotes)`,
+        question: ``,
+        answer: `$("#intro");`,
+        timeCorrect: 0
+        }, {
+        questionNum: 3,
+        questionInstructions: `Select all elements with class "intro": (Use Double Quotes)`,
+        question: ``,
+        answer: `$(".intro");`,
+        timeCorrect: 0
+        }, {
+        questionNum: 4,
+        questionInstructions: `Select all elements with class "intro", "demo" or "end": (Use Double Quotes)`,
+        question: ``,
+        answer: `$(".intro, .demo, .end");`,
+        timeCorrect: 0
+        }, {
+        questionNum: 5,
+        questionInstructions: `Select all <p> elements: (Use Double Quotes)`,
+        question: ``,
+        answer: `$("p");`,
+        timeCorrect: 0
+        }, {
+        questionNum: 6,
+        questionInstructions: `Select the first <p> element: (Use Double Quotes)`,
+        question: ``,
+        answer: `$("p:first");`,
+        timeCorrect: 0
+        }, {
+        questionNum: 7,
+        questionInstructions: `Select the last <p> element: (Use Double Quotes)`,
+        question: ``,
+        answer: `$("p:last");`,
+        timeCorrect: 0
+        }, {
+        questionNum: 8,
+        questionInstructions: `Select every other (even) <tr> element: (Use Double Quotes)`,
+        question: ``,
+        answer: `$("tr:even");`,
+        timeCorrect: 0
+        }]
+    var Current = 0
+    var index = 0
 
-//Set the value of the html to the values of the object.
-
-$('.instructions').text(instructions);
-$('#editor').text(questions);
-
-
-
-$('.test').on('click', function () {
-    var attempt = $('.ace_content').text();
-    var attempt = trimmer(attempt);
-
-
-
-    console.log(attempt);
-    if (attempt === answers) {
-        console.log('Correct!');
-    } else {
-        console.log('Not Correct');
+    /////////////////////Setting the stage
+    //Custom Functions
+    function trimmer(value) {
+        value = value.replace(/\s/g, '');
+        value = value.replace(/[\t\n]+/g, '')
+        value = value.replace(/(\r\n|\n|\r)/gm, '')
+        return value;
     }
-})
+    // Next array item
+    function nextItem() {
+        if (Current == question.length - 1)
+            Current = 0
+        else
+            Current++;
+        console.log(Current);
+        return question[Current];
+    }
+
+    ////////////////////////////ace.js dependencies
+    var editor = ace.edit("editor");
+    //editor.setTheme("ace/theme/twilight");
+    editor.session.setMode("ace/mode/javascript");
+
+    // Appears to only work with php
+    // var beautify = ace.require("ace/ext/beautify"); // get reference to extension
+    // beautify.beautify(editor.session);
+    function beatify() {
+        var val = editor.session.getValue();
+        //Remove leading spaces
+        var array = val.split(/\n/);
+        array[0] = array[0].trim();
+        val = array.join("\n");
+        //Actual beautify (prettify) 
+        val = js_beautify(val);
+        //Change current text to formatted text
+        editor.session.setValue(val);
+    }
+    beatify();
+    $('.test').on('click', function () {
+        beatify();
+    })
+
+    var editor2 = ace.edit("editor2");
+    //editor2.setTheme("ace/theme/twilight");
+    editor2.session.setMode("ace/mode/javascript");
+
+    function beatify2() {
+        var val = editor2.session.getValue();
+        //Remove leading spaces
+        var array = val.split(/\n/);
+        array[0] = array[0].trim();
+        val = array.join("\n");
+        //Actual beautify (prettify) 
+        val = js_beautify(val);
+        //Change current text to formatted text
+        editor2.session.setValue(val);
+    }
+    beatify2();
+    $('.test').on('click', function () {
+        beatify2();
+    })
+
+    var attemptNumber = 0;
+    console.log('attemptNumber: ' + attemptNumber);
+
+    /////////////////////// Here is where the content becomes dynamic
+    function loadQuestion(index) {
+        console.log("Inside loadQuestion attemptNumber:" + attemptNumber);
+        // Display the first question
+        var currentInstruction = question[index].questionInstructions
+        var currentQuestion = question[index].question
+        var currentAnswer = question[index].answer
+        var timeCorrect = question[index].timeCorrect
+
+        var currentInstruction = $.trim(currentInstruction);
+        var currentQuestion = trimmer(currentQuestion);
+        var currentAnswer = trimmer(currentAnswer);
+
+        // Sort Elements by timeCorrect
+        if (index === 6) {
+            console.log('Sorting Incorrect Answers')
+            question.sort(function (a, b) {
+                if (a.timeCorrect < b.timeCorrect) return -1;
+                if (a.timeCorrect > b.timeCorrect) return 1;
+                return 0;
+            });
+        }
+
+
+        console.log(currentInstruction);
+        console.log(currentQuestion);
+        console.log(currentAnswer);
+
+
+        //Set the value of the html to the values of the object.
+        $('.instructions').text(currentInstruction);
+        editor.insert(currentQuestion);
+        $('.result').text('');
+        $('.next').css('display', 'none');
+        $('.submit').css('display', 'none');
+        $('.test').unbind('click').on('click', function () { ////////NOTE: unbinding is a major issue I do not understand here.
+            //Reset Border
+            $('.result').text('');
+            //Getting the content from ace box
+            var attempt = $('#editor .ace_content').text();
+            var attempt = trimmer(attempt);
+            console.log(attempt);
+            editor2.setValue("");
+
+
+
+
+            //Doing check
+            if (attempt === currentAnswer) {
+                console.log('Correct!');
+                //Change result color and text based on result
+                $('.result').css('background-color', 'green');
+                $('#editor').css('border', 'solid 2px green');
+                $('.result').text('Correct!');
+                $('.next').css('display', 'inline');
+                question[index].timeCorrect = question[index].timeCorrect + 1;
+                $('.test').hide();
+                $('.submit').css('display', 'none');
+
+            } else {
+                console.log('Not Correct');
+                //Change result color and text based on result
+                $('.result').css('background-color', '#ff8566');
+                $('.result').text('incorrect, give it one more attempt');
+                $('#editor').css('border', 'solid 2px #ff8566');
+                question[index].timeCorrect = question[index].timeCorrect - 1;
+                console.log(question[index].timeCorrect)
+                attemptNumber += 1;
+                console.log("Inside attemptNumber:" + attemptNumber);
+                if (attemptNumber > 1) {
+                    $('.result').css('background-color', '#80dfff');
+                    $('#editor').css('border', 'solid 2px #80dfff');
+                    $('.result').text('incorrect, below is the correct answer');
+                    editor2.insert(question[index].answer);
+                    beatify2();
+                    $('.submit').css('display', 'inline');
+                    $('.test').hide();
+                    $('.submit').on('click', function () {
+                        $('.result').text('');
+                        //Getting the content from ace box
+                        var attempt = $('#editor .ace_content').text();
+                        var attempt = trimmer(attempt);
+                        console.log(attempt);
+                        if (attempt === currentAnswer) {
+                            console.log('Correct!');
+                            //Change result color and text based on result
+                            $('.result').css('background-color', 'green');
+                            $('#editor').css('border', 'solid 2px green');
+                            $('.result').text('Correct!');
+                            $('.next').css('display', 'inline');
+                            $('.test').hide();
+                            $('.submit').css('display', 'none');
+                        }
+                    });
+                }
+            }
+        });
+    }
+    loadQuestion(index)
+    $('.next').on('click', function () {
+        editor.setValue("")
+        editor2.setValue("");
+        index += 1;
+        console.log(index);
+        attemptNumber = 0;
+        $('.test').show();
+        $('#editor').css('border', '');
+
+
+        if (index <= 6) {
+            loadQuestion(index)
+        } else {
+            console.log('Reseting Index')
+            index = 0;
+            loadQuestion(index)
+        }
+        beatify();
+
+    });
+
+})()
--- scripts.js
+++ scripts.js
@@ -19,4 +19,27 @@ $(document).ready(function () {
         editor.session.setValue(val);
     }
     beatify();
+    $('.show').on('click', function () {
+        beatify();
+    })
+
+    var editor2 = ace.edit("editor2");
+    //editor2.setTheme("ace/theme/twilight");
+    editor2.session.setMode("ace/mode/javascript");
+
+    function beatify2() {
+        var val = editor2.session.getValue();
+        //Remove leading spaces
+        var array = val.split(/\n/);
+        array[0] = array[0].trim();
+        val = array.join("\n");
+        //Actual beautify (prettify) 
+        val = js_beautify(val);
+        //Change current text to formatted text
+        editor2.session.setValue(val);
+    }
+    beatify2();
+    $('.test').on('click', function () {
+        beatify2();
+    })
 });
