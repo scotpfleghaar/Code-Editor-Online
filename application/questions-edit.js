9f174bb69da0377e3510144a943af05f264ae09b
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-12-01 15:57:33 -0600

    Add Questions
    Can now add questions to the DOM

--- application/questions-edit.js
+++ application/questions-edit.js
@@ -192,7 +192,6 @@ $(document).ready(function () {
                 <div class="col-md-10 col-md-offset-1">
                 <h2>Explination:</h2>
                     <div  class="alert alert-info explination"><pre contenteditable="true"><xmp>${currentMCexplination}</xmp></pre></div>
-                    <!-- <input onclick='responsiveVoice.speak($(".explination").text());' type='button' value='🔊 Play' /> -->
                 </div>
                 <div class="col-md-10 col-md-offset-1">
                 <h2>Multiple Choice Section:</h2>
@@ -212,8 +211,6 @@ $(document).ready(function () {
         </div>`;
         $('.qbank').append(insertQuestions);
 
-        arrayOfQuestions.push(insertQuestions);
-
         var editor = ace.edit("editor" + j);
         editor.session.setOption("useWorker", false);
         editor.insert(currentQuestion);
@@ -223,12 +220,213 @@ $(document).ready(function () {
         editorAnswer.insert(currentAnswer);
     }
 
-    $('.qbank').append(`<div class="col-sm-10 col-sm-offset-1 total-details">
+    $('.qbank').append(`<div class="col-sm-10 col-sm-offset-1 add-button total-details">
         <div style="text-align: center;">
-            <button class='btn btn-lg'>Add Question</button>
+            <button class='btn btn-lg add-question-edit'>Add Question</button>
         </div>
     </div>`);
 
+    $('.add-question-edit').off('click').on('click', function () {
+        var j = (($('.editor').length / 2) + 1);
+        $('.add-button').before(`
+        <div class="container total-details">
+        <div class="row">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <hr>
+                    <h1 style="text-align: center;">Question ${(j)}</h1>
+                </div>
+
+                <div class="col-md-10 col-md-offset-1">
+
+                    <div class="wrapper">
+                    <h2>Indstructions:</h2>
+                        <div>
+                            <pre contenteditable="true" class="instructions"><xmp></xmp></pre>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 editor-display">
+                            <h2>Starter Code:</h2>
+
+                            <!-- Create a simple CodeMirror instance -->
+                            <div>
+                            <pre id="editor${j }" class="editor" style="height: 300px;"></pre>
+                            </div>
+                            
+                           
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <p>
+                                    <small class="programmingLanguage"></small>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                        </div>
+                        <p></p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                <h2>Answer Code:</h2>
+                <pre id="editorAnswer${j}" class="editor" style="height: 300px;"></pre>
+                </div>
+                <div class="col-md-10 col-md-offset-1">
+                <h2>Code Challenge Language:</h2>
+                    <label>Select Language
+                        <select id="ace-mode">
+                            <option value="ABAP">ABAP</option>
+                            <option value="ActionScript">ActionScript</option>
+                            <option value="ADA">ADA</option>
+                            <option value="Apache Conf">Apache Conf</option>
+                            <option value="AsciiDoc">AsciiDoc</option>
+                            <option value="Assembly x86">Assembly x86</option>
+                            <option value="AutoHotKey">AutoHotKey</option>
+                            <option value="BatchFile">BatchFile</option>
+                            <option value="C/C++">C/C++</option>
+                            <option value="C#">C#</option>
+                            <option value="C9 Search Results">C9 Search Results</option>
+                            <option value="Cirru">Cirru</option>
+                            <option value="Clojure">Clojure</option>
+                            <option value="Cobol">Cobol</option>
+                            <option value="CoffeeScript">CoffeeScript</option>
+                            <option value="ColdFusion">ColdFusion</option>
+                            <option value="CSS">CSS</option>
+                            <option value="Curly">Curly</option>
+                            <option value="D">D</option>
+                            <option value="Dart">Dart</option>
+                            <option value="Diff">Diff</option>
+                            <option value="Dockerfile">Dockerfile</option>
+                            <option value="Dot">Dot</option>
+                            <option value="EJS">EJS</option>
+                            <option value="Erlang">Erlang</option>
+                            <option value="Forth">Forth</option>
+                            <option value="FreeMarker">FreeMarker</option>
+                            <option value="Gherkin">Gherkin</option>
+                            <option value="Gitignore">Gitignore</option>
+                            <option value="Glsl">Glsl</option>
+                            <option value="Go">Go</option>
+                            <option value="Groovy">Groovy</option>
+                            <option value="HAML">HAML</option>
+                            <option value="Handlebars">Handlebars</option>
+                            <option value="Haskell">Haskell</option>
+                            <option value="haXe">haXe</option>
+                            <option value="HTML" selected>HTML</option>
+                            <option value="HTML (Ruby)">HTML (Ruby)</option>
+                            <option value="INI">INI</option>
+                            <option value="Jack">Jack</option>
+                            <option value="Jade">Jade</option>
+                            <option value="Java">Java</option>
+                            <option value="JavaScript">JavaScript</option>
+                            <option value="JSON">JSON</option>
+                            <option value="JSONiq">JSONiq</option>
+                            <option value="JSP">JSP</option>
+                            <option value="JSX">JSX</option>
+                            <option value="Julia">Julia</option>
+                            <option value="LaTeX">LaTeX</option>
+                            <option value="LESS">LESS</option>
+                            <option value="Liquid">Liquid</option>
+                            <option value="Lisp">Lisp</option>
+                            <option value="LiveScript">LiveScript</option>
+                            <option value="LogiQL">LogiQL</option>
+                            <option value="LSL">LSL</option>
+                            <option value="Lua">Lua</option>
+                            <option value="LuaPage">LuaPage</option>
+                            <option value="Lucene">Lucene</option>
+                            <option value="Makefile">Makefile</option>
+                            <option value="Markdown">Markdown</option>
+                            <option value="MATLAB">MATLAB</option>
+                            <option value="MEL">MEL</option>
+                            <option value="MUSHCode">MUSHCode</option>
+                            <option value="MySQL">MySQL</option>
+                            <option value="Nix">Nix</option>
+                            <option value="Nix">Nix</option>
+                            <option value="Objective-C">Objective-C</option>
+                            <option value="OCaml">OCaml</option>
+                            <option value="Pascal">Pascal</option>
+                            <option value="Perl">Perl</option>
+                            <option value="pgSQL">pgSQL</option>
+                            <option value="PHP">PHP</option>
+                            <option value="Plain Text">Plain Text</option>
+                            <option value="Powershell">Powershell</option>
+                            <option value="Prolog">Prolog</option>
+                            <option value="Properties">Properties</option>
+                            <option value="Protobuf">Protobuf</option>
+                            <option value="Python">Python</option>
+                            <option value="R">R</option>
+                            <option value="RDoc">RDoc</option>
+                            <option value="RHTML">RHTML</option>
+                            <option value="Ruby">Ruby</option>
+                            <option value="Rust">Rust</option>
+                            <option value="SASS">SASS</option>
+                            <option value="SCAD">SCAD</option>
+                            <option value="Scala">Scala</option>
+                            <option value="Scheme">Scheme</option>
+                            <option value="SCSS">SCSS</option>
+                            <option value="SH">SH</option>
+                            <option value="SJS">SJS</option>
+                            <option value="Smarty">Smarty</option>
+                            <option value="snippets">snippets</option>
+                            <option value="Soy Template">Soy Template</option>
+                            <option value="Space">Space</option>
+                            <option value="SQL">SQL</option>
+                            <option value="Stylus">Stylus</option>
+                            <option value="SVG">SVG</option>
+                            <option value="Tcl">Tcl</option>
+                            <option value="Tex">Tex</option>
+                            <option value="Text">Text</option>
+                            <option value="Textile">Textile</option>
+                            <option value="Toml">Toml</option>
+                            <option value="Twig">Twig</option>
+                            <option value="Typescript">Typescript</option>
+                            <option value="Vala">Vala</option>
+                            <option value="VBScript">VBScript</option>
+                            <option value="Velocity">Velocity</option>
+                            <option value="Verilog">Verilog</option>
+                            <option value="XML">XML</option>
+                            <option value="XQuery">XQuery</option>
+                            <option value="YAML">YAML</option>
+                        </select>
+                    </label>
+                </div>
+
+                <div class="col-md-10 col-md-offset-1">
+                <h2>Explination:</h2>
+                    <div  class="alert alert-info explination"><pre contenteditable="true"><xmp></xmp></pre></div>
+                </div>
+                <div class="col-md-10 col-md-offset-1">
+                <h2>Multiple Choice Section:</h2>
+                    <ul class="choices list-group">
+                        <li class="choice list-group-item"><small>Answer 1: </small><pre contenteditable="true"><xmp></xmp></pre></li>
+                        <li class="choice list-group-item"><small>Answer 2: </small><pre contenteditable="true"><xmp></xmp></pre></li>
+                        <li class="choice list-group-item"><small>Choice 1: </small><pre contenteditable="true"><xmp></xmp></pre></li>
+                        <li class="choice list-group-item"><small>Choice 2: </small><pre contenteditable="true"><xmp></xmp></pre></li>
+                        <li class="choice list-group-item"><small>Choice 3: </small><pre contenteditable="true"><xmp></xmp></pre></li>
+                        <li class="choice list-group-item"><small>Choice 4: </small><pre contenteditable="true"><xmp></xmp></pre></li>
+                        <li class="choice list-group-item"><small>Choice 5: </small><pre contenteditable="true"><xmp></xmp></pre></li>
+                    </ul>
+                    <hr>
+                </div>
+            </div>
+        </div>
+        </div>`);
+
+        var editor = ace.edit("editor" + j);
+        editor.session.setOption("useWorker", false);
+        editor.insert('');
+
+        var editorAnswer = ace.edit("editorAnswer" + j);
+        editorAnswer.session.setOption("useWorker", false);
+        editorAnswer.insert('');
+
+
+
+
+    });
 
 
 
